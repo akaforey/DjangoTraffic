2bae31fe87f6eee6935e6f8d2fff4cd39782d76d
Alex Kaforey <akafore1@binghamton.edu>
2023-10-25 15:21:10 -0600

    def get_traffic, added initial tomtom api support

--- DjangoWebProject1/traffic/models.py
+++ DjangoWebProject1/traffic/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 import datetime
+import json
+from ..DjangoWebProject1.secret import API_KEY
+import requests
 
 # Create your models here.
 
@@ -10,12 +13,53 @@ class Address(models.Model):
     class Meta:
         verbose_name_plural = "Addresses"
 
+
+def get_traffic(start, end):
+    baseURL = "api.tomtom.com"
+    versionNumber = "2"
+    key = API_KEY
+    
+    postURL = f'https://{baseURL}/routemonitoring/{versionNumber}/routes?key={key}'
+
+    body = {
+        "name": "test",
+        "pathPoints": [
+            {"longitude": start.longitude, "latitude": start.latitude},
+            {"longitude": end.longitude, "latitude": end.latitude}
+        ]
+    }
+    json_body = json.dumps(body)
+    
+    try:
+        post_response = requests.post(url=postURL, data=body)
+        print(post_response.text)
+        print(post_response.json())
+        routeID = json.loads(post_response.json())["routeID"]
+        getURL = f'https://{baseURL}/routemonitoring/{versionNumber}/routes/{routeID}?key={API_KEY}'
+        get_response = requests.get(url=getURL)
+        print(get_response.json())
+    except requests.exceptions.JSONDecodeError as e:
+        print(e)
+        return None
+        
+    return json.loads(get_response.json())["travelTime"]
+    
+
+    
+    
+    
+    
+    
+    
+
+
 class TrafficReport(models.Model):
     start_location = models.ForeignKey(Address, on_delete=models.SET_NULL, related_name='traffic_start', null=True)
     end_location = models.ForeignKey(Address, on_delete=models.SET_NULL, related_name='traffic_end', null=True)
     created = models.DateTimeField(auto_now_add=True)
     
     travel_time = datetime.timedelta(minutes=14, seconds=30)
+    # travel_time = datetime.timedelta(seconds=get_traffic(start_location, end_location)
     duration = models.DurationField(editable=False, default=travel_time, auto_created=True)
 
     
